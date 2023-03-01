3a03929142023805cf7cd83a632c0941868a8174
Averlan Allan <allan.averlan@laposte.net>
2023-03-01 14:53:25 +0100

    modif css

--- src/components/Displaybody.jsx
+++ src/components/Displaybody.jsx
@@ -11,7 +11,7 @@ const Displaybody = () => {
             <h1 className='text-shark-text text-center text-xl lg:text-3xl w-full mt-1  top-1 '>En ce moment</h1>
            <Movie />;    
             </div>
-        <div className="secondarydisplay container m-auto antiscrollbar pt-6 h-72 flex flex-row flex-wrap justify-around overflow-y-scroll">
+        <div className="secondarydisplay container m-auto antiscrollbar pt-6  flex flex-row flex-wrap justify-around overflow-y-scroll">
                 <h2 className='text-center text-shark-text top-0'></h2>
                <Actor />
             </div>
