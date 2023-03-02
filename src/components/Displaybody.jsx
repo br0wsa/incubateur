532ea71181d4b955543629921fd1fe1fa35af93b
Averlan Allan <allan.averlan@laposte.net>
2023-03-02 10:31:16 +0100

    details movie and actor

--- src/components/Displaybody.jsx
+++ src/components/Displaybody.jsx
@@ -7,8 +7,8 @@ const Displaybody = () => {
     return (
 
         <>
-        <div className="primardisplay pt-10 h-72 flex flex-row flex-wrap justify-around bg-shark-900 overflow-y-scroll">
-            <h1 className='text-shark-text text-center text-xl lg:text-3xl w-full mt-1  top-1 '>En ce moment</h1>
+        <div className="primardisplay pt-10 h-80 flex flex-row flex-wrap justify-around bg-shark-900 overflow-x-scroll">
+            <h1 className='text-shark-text text-center text-xl lg:text-3xl w-full mt-1 top-1 '>En ce moment</h1>
            <Movie />;    
             </div>
         <div className="container m-auto pt-6  flex flex-row flex-wrap justify-around">
--- src/pages/DetailsActor.jsx
+++ src/pages/DetailsActor.jsx
@@ -2,37 +2,50 @@ import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from "react";
 import{ useParams} from "react-router-dom";
+import DetailsMovie from "../components/DetailsMovie"
 
 
 
-const Details = () => {
-    const [data, setData] = useState([]);
+const DetailsActor = () => {
+    const [movies, setMovie] = useState([]);
+    const [actors, setActors] = useState([]);
     const params = useParams();
+    
+    const fetchData = () => {
+        const actors = axios.get(`https://api.themoviedb.org/3/person/${params.id}?api_key=836c908c992e085a541e9c67774163c7&language=fr-EU`); /* Recup film par ID*/
+        const movies = axios.get(`https://api.themoviedb.org/3/person/${params.id}/movie_credits?api_key=836c908c992e085a541e9c67774163c7&language=fr-EU`); /* Recup Acteur par ID du film */ 
+        axios.all([actors,movies]).then(
+            axios.spread((...allData) =>{
+            const allDataActor = allData[0];
+            const allDataMovie = allData[1];
+    
+            setActors(allDataActor.data);
+            setMovie(allDataMovie.data['cast']);
+            console.log(allDataMovie.data);
+        }))
+    }
 
-    useEffect(() => {
-        axios
-            .get(`https://api.themoviedb.org/3/person/${params.id}?api_key=836c908c992e085a541e9c67774163c7&language=fr-EU`)
-            .then((res) => {
-                console.log(res.data);
-                setData(res.data);
-            });
+    useEffect (() => {
+        fetchData()
     }, [])
 
  return (
     <div> 
             <div className='container m-auto mb-5 '>
-            <h2 className='text-xl pb-5'><p>{data.name}</p></h2>
+            <h2 className='text-xl pb-5'><p>{actors.name}</p></h2>
                 <div className='flex'>
-                    <img className='rounded-[8px] w-96' src={"https://image.tmdb.org/t/p/original" + data.profile_path} alt={data.original_title} />
+                    <img className='rounded-[8px] w-96' src={"https://image.tmdb.org/t/p/original" + actors.profile_path} alt={actors.original_title} />
                     <div className=' mx-5 p-1 bg-shark-900 rounded-[8px] '>
                         <p className='underline font-bold text-lg mb-5'> Biographie :</p>
-                        <p>{data.biography}</p>
+                        <p>{actors.biography}</p>
                     </div>
                 </div>
                 <div>
                     <p className='underline font-bold text-lg '>Apparition :</p>
-                    <div>
-                        <img className='actor' src={"https://image.tmdb.org/t/p/original" + data.backdrop_path} alt={data.original_title} />
+                    <div  className='actor flex gap-5 overflow-hidden  flex-wrap'>
+                    {movies.map((movie) => (
+                        <DetailsMovie key={movie.id} movie={movie} />
+                ))}
                     </div>
                 </div>
             </div>
@@ -40,6 +53,6 @@ const Details = () => {
  );
 };
 
-export default Details;
+export default DetailsActor;
 
 
