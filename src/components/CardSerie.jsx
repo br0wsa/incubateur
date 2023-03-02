0653a41ec26f1cbe5d7bb135072f59f351c6c230
Averlan Allan <allan.averlan@laposte.net>
2023-03-02 15:58:58 +0100

    modif serie

--- src/components/CardSerie.jsx
+++ src/components/CardSerie.jsx
@@ -6,11 +6,11 @@ function CardSerie({ serie }) {
         <div className="infos w-64">
             <Link to={`/serie/${serie.id}`}>
                 <div className=" duration-700 hover:scale-95 cursor-pointer">
+                <h2 className='text-center'>{serie.name}</h2>
                     <img className='rounded-[8px] w-440 h-660 m-2 ' src={"https://image.tmdb.org/t/p/original" + serie.backdrop_path} alt={serie.original_title} />
                 </div>
                 <div className="mov">
-                    <h2><p>{serie.name}</p></h2>
-                    <p>{serie.first_air_date + " | " + serie.vote_average + "/10 (" + serie.vote_count + ")"}</p>
+           
                 </div>
             </Link>
         </div>
