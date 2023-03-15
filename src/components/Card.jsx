6b6cc383402513991224ae530f6dfbbc00845f06
Averlan Allan <allan.averlan@laposte.net>
2023-03-15 16:21:31 +0100

    modif style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { format } from 'date-fns'
+import ComingSoon from "../assets/images/cs.png";
 import { Link } from 'react-router-dom';
 
 function Card({ movie }) {
     return (
-        <div className="infos w-64 ">
-            <Link to={`/movie/${movie.id}`}>
-                <div className=" duration-700 hover:scale-95 cursor-pointer">
-                    <img className='rounded-[8px] w-440 h-660 m-2 ' src={"https://image.tmdb.org/t/p/original" + movie.backdrop_path} alt={movie.original_title} />
-                </div>
-                <div className="mov">
-                    <h2><p>{movie.title}</p></h2>
-                    <p>{ + " | " + movie.vote_average + "/10 (" + movie.vote_count + ")"}</p>
-                </div>
-            </Link>
-        </div>
+        <div className="infos w-64">
+        <Link to={`/movie/${movie.id}`}>
+            <div className=" duration-700 hover:scale-95 cursor-pointer">
+            <h2 className='text-center'>{movie.title}</h2>
+                <img className='rounded-[8px] w-440 h-660 m-2 ' src={movie.backdrop_path == null ? ComingSoon : "https://image.tmdb.org/t/p/original" + movie.backdrop_path  } />
+            </div>
+            <div className="mov">
+            </div>
+        </Link>
+    </div>
     );
 }
 // probleme avec le format date 
